20c57c6be29036dd905691997c6b59c460090851
youngjae459 <youngjae459@gmail.com>
2023-02-23 09:54:56 +0900

    main 추가

---
+++ src/main/java/com/pbl/level0/p120910/Main.java
@@ -0,0 +1,14 @@
+package com.pbl.level0.p120910;
+
+public class Main {
+    public static void main(String[] args) {
+        new Solution().solution(2, 10);
+    }
+}
+
+class Solution {
+    public int solution(int n, int t) {
+        int answer = 0;
+        return answer;
+    }
+}
