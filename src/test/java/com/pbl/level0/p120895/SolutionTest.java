51f96ece6a062b2565f70f28c75428516abcf64c
youngjae459 <youngjae459@gmail.com>
2023-03-05 16:21:31 +0900

    2023.03.05 프로그래머스 문제풀이

---
+++ src/test/java/com/pbl/level0/p120895/SolutionTest.java
@@ -0,0 +1,23 @@
+package com.pbl.level0.p120895;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    @DisplayName("hello, 1, 2 -> hlelo")
+    void t1() {
+        assertThat(new Solution().solution("hello", 1, 2)).isEqualTo("hlelo");
+    }
+
+    @Test
+    @DisplayName("I love you, 3, 6	-> I l veoyou")
+    void t2() {
+        assertThat(new Solution().solution("I love you", 3, 6)).isEqualTo("I l veoyou");
+    }
+
+}
